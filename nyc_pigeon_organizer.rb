4222fd205495463086775f50518f5644f483e18a
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-07 21:34:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  n_h = {} # new hash
+  data.each_pair do |attributes, i_h| #i_h = inner hash
+    i_h.each_pair do |i_a, name| #i_a = inner attribute
+      names.each{|name|
+        if !n_h[name]
+          n_h[name] = {}
+        end
+        if data[attributes][i_a].include?(name)
+          if !n_h[name][attributes]
+            n_h[name][attributes] = []
+          end
+          n_h[name][attributes].append(i_a.to_s)
+        end
+      }
+    end
+  end
+  n_h 
 end
