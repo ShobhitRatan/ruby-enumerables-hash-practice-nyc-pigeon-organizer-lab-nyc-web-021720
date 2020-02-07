d26dd1bf09d654dc1bbbd0a48e44838067dd7bfe
Shobhit Ratan <shobhit.ratan@baruchmail.cuny.edu>
2020-02-07 22:07:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,44 +1,45 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  n_h = {} # new hash
-  array = []
-  p_m = data[:gender][:male] #male pigeon
-  p_f = data[:gender][:female] #female pigeon
-  n = 0
-  while n < p_m.length do
-    array.append(p_m[n])
-    n += 1
-  end
-  n = 0
-  while x < p_f.length do
-    array.append(p_f[n])
-    n += 1
-  end
-  array.each {|n| n_h[n] = {
-    :color => [],
-    :gender => [],
-    :lives => []
+
+  new_hash = {}
+    array = []
+    pigeon_male = data[:gender][:male]
+    pigeon_female = data[:gender][:female]
+    i = 0
+    while i < pigeon_male.length do
+      array.push(pigeon_male[i])
+      i += 1
+    end
+    i = 0
+    while i < pigeon_female.length do
+      array.push(pigeon_female[i])
+      i +=1
+    end
+    array.each {|i| new_hash[i] = {
+      :color => [],
+      :gender => [],
+      :lives => []
     }}
-  # puts n_h
-  # n_h
-  n_h.each do |hash_name, hash_name_hash|
-    hash_name_hash.each do |new_inner_hash_key, new_hash_inner_array|
-      data.each do |data_key_hash_1, data_hash_inner_hash|
-        data_hash_inner_hash.each do |data_hoh_key, data_hoh_name_array|
-          data_hoh_name_array.each do |name|
-            hash_name_str = hash_name.to_s
-            if name == hash_name_str && data_key_hash_1 == new_inner_hash_key
-              attribute = data_hoh_key.to_s
-              new_hash_inner_array.append(attribute)
-              puts new_hash_inner_array
+    #puts new_hash
+    #new_hash
+    new_hash.each do |hash_name, hash_name_hash|
+      hash_name_hash.each do |new_inner_hash_key, new_hash_inner_array|
+        data.each do |data_key_hash1, data_hash_inner_hash|
+          data_hash_inner_hash.each do |data_hoh_key, data_hoh_name_array|
+            data_hoh_name_array.each do |name|
+              hash_name_str = hash_name.to_s
+              if name == hash_name_str && data_key_hash1 == new_inner_hash_key
+                attribute = data_hoh_key.to_s
+                new_hash_inner_array << attribute
+                puts new_hash_inner_array
+              end
             end
           end
         end
       end
     end
-  end
-  puts n_h
-  n_h
+    puts new_hash
+    new_hash
 end
 
 pigeon_data = {
@@ -63,4 +64,4 @@ pigeon_data = {
 #puts pigeon_data[:gender][:male][0]
 #puts pigeon_data[:gender][:female][0]
 
-nyc_pigeon_organizer(pigeon_data) 
+nyc_pigeon_organizer(pigeon_data)
